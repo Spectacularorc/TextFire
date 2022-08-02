4baceac800eff283f1f7a1e88732dcf6fa5e2544
Spectacularorc <107068479+Spectacularorc@users.noreply.github.com>
2022-08-02 22:47:11 +0530

    Updates

---
+++ precache-manifest.ba9875cbbfa0da3a8cbdeb31e71414f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d4e2d3eac8b2c2b8b72",
+    "url": "/TextFire/static/css/main.216318ce.chunk.css"
+  },
+  {
+    "revision": "6d4e2d3eac8b2c2b8b72",
+    "url": "/TextFire/static/js/main.6d4e2d3e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextFire/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextFire/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "b8488646653547c2229d",
+    "url": "/TextFire/static/js/runtime~main.b8488646.js"
+  },
+  {
+    "revision": "799aa48b18e460c82e97a5a003ac1ce6",
+    "url": "/TextFire/index.html"
+  }
+];
